c96e43bc98d03d59459e757d3477c28208d895e3
Kresak <78959259+Kresak@users.noreply.github.com>
2021-04-07 14:53:14 +0300

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,42 @@
+import math
+import random
+print("Ноль в качестве знака операции завершит работу программы")
+while True:
+    s = input("Знак (+, -, *, /, ^, "
+              "модуль, !, арккосинус, рандом): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/', '^'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Деление на ноль!")
+        elif s == '^':
+            print(math.pow(x, y))
+
+    elif s in ("модуль", "!", "арккосинус"):
+        x = float(input("x="))
+        if s == "модуль":
+            print(abs(x))
+        elif s == "!":
+            print(math.factorial(x))
+        elif s == "арккосинус":
+            if x > 1 or x < -1:
+                print("Ваше число не принадлежит промежутку от -1 до 1")
+                continue;
+            else:
+                print(math.acos(x))
+    elif s == "рандом":
+        print(random.uniform(0, 1000))
+    else:
+        print("Неверный знак операции!")
+    print("\n")
