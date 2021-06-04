69c1ec6c31a1f2dc23be3c6e53e974d66f455fe2
Kresak <78959259+Kresak@users.noreply.github.com>
2021-06-04 18:33:46 +0300

    Update Task_6(5).py

--- Task_6(5).py
+++ Task_6(5).py
@@ -1,16 +1,22 @@
-def fun_slova():
+def f_spisok():
     return set(input("Введите слова через запятую (без пробелов): ").split(',')) # set - множество
-def fun_kolvo(set):
+
+
+def f_kolvo(set):
     return len(set)
-def fun_slovar(keys, values):
-    return slovar(zip(keys, values)) # функция zip позволяет пройтись одновременно по нескольким итерируемым объектам (спискам и др.)
 
-klyuchi = fun_slova()
-print(fun_kolvo(klyuchi))
-znacheniya = fun_slova()
 
-if len(klyuchi) == len(znacheniya):
-    slovar = fun_slovar(zip(klyuchi, znacheniya)) # создаём словарь из двух массивов
+def f_slovar(keys, values):
+    return dict(zip(keys, values)) # функция zip позволяет пройтись одновременно по нескольким итерируемым объектам (спискам и др.)
+
+
+keys = f_spisok()
+print(f_kolvo(keys))
+values = f_spisok()
+
+
+if len(keys) == len(values):
+    slovar = f_slovar(keys, values)  # создаём словарь из двух массивов
     print(slovar)
 else:
-    print("Ошибка: Ошибка!")
+    print("Ошибка: Количество слов в списках не совпадает!")
